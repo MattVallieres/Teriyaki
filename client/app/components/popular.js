62a695a4b4c44b3e33b16932a2c9f001c78c987e
Matthew Vallieres <mvallieres@hotmail.ca>
2023-09-02 14:08:41 -0800

    popular

--- client/app/components/popular.js
+++ client/app/components/popular.js
@@ -1,25 +1,26 @@
-"use client"
+"use client";
+import React, { useState, useEffect } from "react";
 import { BiRightArrow } from "react-icons/bi";
-import { useState, useEffect } from "react";
 import { FiLoader } from 'react-icons/fi';
 import Link from "next/link";
 
-export const Trending = () => {
-  // We have a list where we keep information about anime
-  const [recommend, setRecommend] = useState([]);
+export const Popular = () => {
+  // state to store info
+  const [getPopular, setGetPopular] = useState([]);
   // When we're waiting to see the anime, we show a spinning icon. When we see the anime, we stop the spinning icon
   const [isLoading, setIsLoading] = useState(true);
 
-  // We fetch the data when the page loads
+  // fetch data when the page loads
   useEffect(() => {
     fetchData();
   }, []);
 
-  // Let's go fetch some cool anime!
+
+  // fetching top anime
   const fetchData = async () => {
     try {
-      // Hey api, give me info about what you'll recommend today!
-      const response = await fetch(`https://api.jikan.moe/v4/recommendations/anime`);
+      // Hey api, what anime is popular?
+      const response = await fetch(`https://api.jikan.moe/v4/top/anime`);
       // Check if the response is successful (status code 200)
       if (!response.ok) {
         throw new Error(`Error fetching recommendations: ${response.status} - ${response.statusText}`);
@@ -28,7 +29,7 @@ export const Trending = () => {
       const data = await response.json();
       // Slice to show only 6 recommendations
       const slicedData = data.data.slice(0, 6);
-      setRecommend(slicedData);
+      setGetPopular(slicedData);
       // We're done fetching data, so set isLoading to false
       setIsLoading(false);
     } catch (error) {
@@ -39,6 +40,7 @@ export const Trending = () => {
     }
   };
 
+
   return (
     <>
       {isLoading ? ( // If isLoading is true, show the loading spinner
@@ -48,30 +50,28 @@ export const Trending = () => {
       ) : (
         <div className="flex justify-center">
           <div className="my-20 px-4 md:px-8">
-            <h1 className="mb-4 uppercase text-lg font-medium md:text-xl">What's trending today!</h1>
+            <h1 className="mb-4 uppercase text-lg font-medium md:text-xl">Popular among the fans!</h1>
             <div className="flex">
-              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-8 w-50">
-                {/* map to show 6 anime */}
-                {recommend.length > 0 &&
-                  recommend.map((x, index) => (
-                    <div key={`${x.entry[1].mal_id}-${index}`} className="w-44">
-                      <Link href={`/anime/${x.entry[0].mal_id}`}>
+              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 w-50">
+                {/* map to show 6 anime  */}
+                {getPopular.length > 0 &&
+                  getPopular.map((x) => (
+                    <div key={x.mal_id} className="w-44">
+                      <Link href={`/anime/${x.mal_id}`}>
                         <img
-                          src={x.entry[1].images.jpg.image_url}
-                          alt={x.entry[1].title}
+                          src={x.images.jpg.image_url}
+                          alt={x.title_english}
                           className="h-60 w-44 transition-transform duration-300 ease-in-out transform-gpu hover:scale-105 hover:opacity-75"
                         />
-                        <h3 className="flex-wrap text-sm my-2">
-                          {/* if the title is too long add ...*/}
-                          {x.entry[1].title.length > 50 ? `${x.entry[1].title.slice(0, 50)}...` : x.entry[1].title}
-                        </h3>
+                        {/* if the title is too long add ...*/}
+                        <h3 className="flex-wrap text-sm my-2">{x.title_english.length > 50 ? `${x.title_english.slice(0, 50)}...` : x.title_english}</h3>
                       </Link>
                     </div>
                   ))}
               </div>
             </div>
             <div className="flex">
-              <Link href="/recommendations" className="text-base font-medium uppercase bg-[#202124] py-2 px-4 mt-10 hover:bg-[#414141] flex items-center">Browse Trendings <BiRightArrow className="ml-2" /></Link>
+              <Link href="/popular" className="text-base font-medium uppercase bg-[#202124] py-2 px-4 mt-10 hover:bg-[#414141] flex items-center">Browse Popularity<BiRightArrow className="ml-2" /></Link>
             </div>
           </div>
         </div>
--- client/app/page.js
+++ client/app/page.js
@@ -1,12 +1,13 @@
 import { Carousel } from "./components/carousel";
 import { Trending } from "./components/trending";
+import { Popular } from "./components/popular";
 
 export default function Home() {
   return (
     <>
       <Carousel />
       <Trending />
-      <h1>Hello world!</h1>
+      <Popular />
     </>
   )
 }
