46b0e17d423bbfdb5266c5614980bbdc43cee899
Matthew Vallieres <mvallieres@hotmail.ca>
2023-09-12 09:34:48 -0800

    navbar done

--- client/app/components/navbar.js
+++ client/app/components/navbar.js
@@ -1,9 +1,9 @@
 "use client"
 import Link from 'next/link';
 import React, { useState, useContext } from 'react';
-import { BiSearchAlt, BiLogOut } from 'react-icons/bi';
+import { BiUser, BiLogOut } from 'react-icons/bi';
 import { GiChicken } from 'react-icons/gi'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
 import { FiBookmark, FiUser } from "react-icons/fi"
 
 
@@ -33,92 +33,77 @@ export const Navbar = () => {
     };
 
     return (
-        <nav className="bg-[#202124] relative font-light z-20 text-neutral-300">
-            <div className="flex mx-auto justify-between">
+        <nav className="bg-neutral-800 z-20 sticky top-0">
+            <div className="flex justify-between">
                 <div className="h-[3.75rem] flex">
                     {/* Hamburger */}
-                    <div onClick={handleNav} className="flex items-center justify-center p-4 text-neutral-300 hover:text-slate-50 duration-200 cursor-pointer md:hidden">
-                        <AiOutlineMenu className="text-xl" />
+                    <div onClick={handleNav} className="flex items-center text-lg px-4 hover:bg-neutral-900 duration-200 cursor-pointer md:hidden">
+                        <AiOutlineMenu />
                     </div>
                     {/* Title */}
-                    <Link href="/" className="flex items-center justify-center uppercase text-orange-400 font-bold text-xl md:text-2xl p-4">Teriyaki<GiChicken className='ml-2 flex hidden md:flex' /></Link>
+                    <Link href="/" className="flex items-center uppercase text-orange-500 font-semibold text-base md:text-xl px-4 hover:text-slate-50 duration-200">Teriyaki Anime<GiChicken className='ml-2 flex hidden md:flex' /></Link>
                 </div>
                 {/* Navigation and Search */}
-                <div className="flex font-medium">
+                <div className="flex">
                     {/* Search */}
-                    <Link href="/search" className="flex items-center px-4 justify-center text-lg hover:text-slate-50 duration-200"><BiSearchAlt /></Link>
-                    <div className="hidden md:flex h-[3.75rem]">
+                    <Link href="/search" className="flex items-center px-4 justify-center text-lg md:text-xl hover:bg-neutral-900 duration-200"><AiOutlineSearch /></Link>
+                    <div className="hidden md:flex text-base">
                         {/* Navigation */}
-                        <Link href="/popular" className="flex px-4 items-center hover:text-slate-50 duration-200">Popular</Link>
-                        <Link href="/recommendation" className="flex px-4 items-center hover:text-slate-50 duration-200">Recommendation</Link>
+                        <Link href="/popular" className="flex px-4 items-center hover:bg-neutral-900 duration-200">Popular</Link>
+                        <Link href="/recommendation" className="flex px-4 items-center hover:bg-neutral-900 duration-200">Recommendation</Link>
                     </div>
 
                     {/* Dropdown and User Navigation */}
                     <div className="flex">
                         {isAuthenticated && currentUser ? (
-                            <button onClick={toggleDropdown} className="px-4 relative">
-                                {/* User Profile Picture */}
-                                <div className="w-9 h-9 rounded-full bg-gray-300 flex items-center justify-center text-gray-600">
-                                    {currentUser.name}
-                                </div>
+                            <button onClick={toggleDropdown} className="px-4 relative hover:bg-neutral-900">
+                                {/* User Profile */}
+                                <BiUser className="text-lg md:text-2xl flex items-center justify-center text-gray-200" />
                             </button>
                         ) : (
-                            <Link href="/login" className="flex px-4 items-center font-medium text-neutral-300 hover:text-slate-50 duration-200">
+                            <Link href="/login" className="flex text-base px-4 items-center">
                                 Login
                             </Link>
                         )}
 
                         {isDropdownOpen && (
-                            <div className="absolute top-[3.75rem] w-60 right-0 bg-[#161616] md:w-72">
-                                <div className="flex p-2 items-center my-2">
-                                    <div className="w-9 h-9 rounded-full bg-gray-300 flex items-center justify-center text-gray-600 font-bold text-sm mr-2">
-                                        {currentUser.name}
-                                    </div>
-                                    <h1 className="flex py-2 items-center block hover:bg-neutral-900"> {currentUser.name} </h1>
+                            <div className="absolute top-16 w-72 right-2 bg-neutral-900 rounded md:w-72 flex flex-col">
+                                <div className="flex items-center ">
+                                    <BiUser className="ml-4 text-xl md:text-2xl flex items-center justify-center text-gray-200" />
+                                    <h1 className="pl-2 py-6 block text-lg">{currentUser.name}</h1>
                                 </div>
-                                    {/* User Navigation */}
-                                    <Link href="/bookmarks" className="flex p-2 items-center hover:bg-neutral-800 hover:text-slate-50 duration-200"><FiBookmark className="mr-2" /> Bookmarks</Link>
-                                    <Link href="/profile" className="flex p-2 items-center hover:bg-neutral-800 hover:text-slate-50 duration-200"><FiUser className="mr-2" /> My Account</Link>
-                                    <span onClick={handleLogout} className="flex p-2 items-center hover:bg-neutral-600 hover:bg-neutral-800 cursor-pointer hover:text-slate-50 duration-200"><BiLogOut className="mr-2" />Logout</span>
-                                </div>
+                                {/* User Navigation */}
+                                <Link href="/bookmarks" className="flex p-2 items-center hover:bg-neutral-800 duration-200"><FiBookmark className="mr-2" />Bookmarks</Link>
+                                <Link href="/profile" className="flex p-2 items-center hover:bg-neutral-800 duration-200"><FiUser className="mr-2" />My Account</Link>
+                                <span onClick={handleLogout} className="flex p-2 items-center hover:bg-neutral-800 cursor-pointer duration-200"><BiLogOut className="mr-2" />Logout</span>
+                            </div>
                         )}
                     </div>
                 </div>
             </div>
 
 
-            {nav && <div className="bg-black/20 fixed w-full h-screen z-10 top-0 right-0"></div>}
+            {nav && <div className="bg-black/20 fixed w-full h-screen z-10 right-0"></div>}
 
             {/* Side Drawer */}
             <div
                 className={
                     nav
-                        ? 'fixed top-0 left-0 w-[250px] h-screen bg-neutral-900 z-10 duration-300'
+                        ? 'fixed top-[3.75rem] left-0 w-[250px] h-screen bg-neutral-900 z-10 duration-300'
                         : 'fixed top-0 right-[-100%] w-[250px] h-screen z-10 duration-300'
                 }
             >
-
-                {/* Close Side Drawer */}
-                <div className="items-center flex p-4">
-                    <h1 onClick={handleNav} className="flex items-center text-xl absolute mt-8 text-md cursor-pointer"><AiOutlineClose /></h1>
-                </div>
-
                 {/* Navigation */}
-                <ul className="pt-10 flex flex-col p-4 text-md ">
-                    <li className="flex text-xs uppercase text-[#818283] font-bold">BROWSE</li>
-                    <li className="py-2 flex hover:text-slate-50 duration-200">
-                        <Link href="/" onClick={handleNav}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className="py-2 flex hover:text-slate-50 duration-200">
+                <ul className="pt-4 p-4">
+                    <li className="text-xs uppercase text-zinc-400 font-bold">BROWSE</li>
+                    <li className="py-2 flex">
                         <Link href="/recommendation" onClick={handleNav}>
                             Recommendation
                         </Link>
                     </li>
-                    <li className="py-2 flex hover:text-slate-50 duration-200">
+                    <li className="py-2 flex">
                         <Link href="/popular" onClick={handleNav}>
-                        Popular
+                            Popular
                         </Link>
                     </li>
                 </ul>
